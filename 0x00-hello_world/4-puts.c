e835f865be087428c295416a3f6b7eb07bbe6d3a
goosoftboss <onyimao@gmail.com>
2022-10-13 17:10:20 -0700

    c work

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * this code prints only strings
+ * it also retturn a 0 value
+ */
+int main(void)
+{
+	/*this function uses puts to display the output:wq*/
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
