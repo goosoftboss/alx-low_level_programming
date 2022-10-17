68efebf1d962f515e5e7a1da8b03e7a9dcf6efa1
goosoftboss <onyimao@gmail.com>
2022-10-17 15:39:33 -0700

    alphabet4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return 0
+ */
+int main(void)
+{
+	int n;
+	int q = 'q';
+	int e = 'e';
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		if (n != q || n != e)
+		{
+			putchar(n);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
