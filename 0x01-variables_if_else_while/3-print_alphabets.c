557a8a6681e79b07654d065aab9fef0bc25fe61a
goosoftboss <onyimao@gmail.com>
2022-10-17 15:14:44 -0700

    alphabet3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 'A'; n <= 'z'; n++)
+	{
+		if (n >= 'Z')
+		{
+		putchar(n);
+		}
+		else
+		{
+		putchar(n);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
