22cdff0621ae056520a5a0ce79e037cc7dbf6236
goosoftboss <onyimao@gmail.com>
2022-10-17 22:04:03 -0700

    comb100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,23 +6,33 @@
 int main(void)
 {
 	int i;
+	int k = '0';
 	int j;
 
-	for (i = 10; i <= 19; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		for (j = 10; j <= 19; j++)
+		for (j = k; j <= '9'; j++)
 		{
-			if ((j % 10) > (i % 10))
+			if (i != j)
 			{
-				putchar((i % 10) + '0');
-				putchar((j % 10) + '0');
-				if (i != 18 || != 19)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				putchar(i);
+				putchar(j);
+			}
+			if (i == j)
+			{
+				continue;
+			}
+			if (i == '8' && j == '9')
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
 			}
 		}
+		k++;
 	}
 	putchar('\n');
 	return (0);
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,9 +5,9 @@
  */
 int main(void)
 {
-	int i;
+	char i;
 
-	for (i = 0; i < 10; i++)
+	for (i = '0'; i < "10"; i++)
 	{
 		putchar(i);
 	}
