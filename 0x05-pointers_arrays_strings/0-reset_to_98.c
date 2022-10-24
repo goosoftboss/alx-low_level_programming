7070cb17d3be0dc58886ffe83f88e426e9bf4d52
goosoftboss <onyimao@gmail.com>
2022-10-24 13:20:52 -0700

    3-puts

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,20 +1,12 @@
-#include <stdio.h>
 #include "main.h"
 /***
- * main - calls the function reset_to_98
  * reset_to_98 - changes value of pointer m to 98
  * Return: 0
  */
-int reset_to_98(int n)
+void reset_to_98(int n)
 {
 	n = 98;
 	int *m = &n;
 
 	_putchar('%p\n', *m);
-}
-
-int main(void)
-{
-	reset_to_98();
-	return (0);
 }
