4b75a0361ffe17cd7bf60843c87103d5d2f36d4c
goosoftboss <onyimao@gmail.com>
2022-11-14 06:04:23 -0800

    dog.h commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/*
+ * struct dog - structure type for a dog
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ * Description: data structure for dog
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void free_dog(dog_t *d);
+#endif
