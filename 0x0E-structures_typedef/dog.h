ba4170e87790724c1768b72021afabbaf213582b
goosoftboss <onyimao@gmail.com>
2022-11-14 06:22:44 -0800

    1-init_dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,13 +6,14 @@
  * @age: Dog's age
  * @owner: Dog's owner
  * Description: data structure for dog
+ * struct dog: structure type for a dog
  */
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 dog_t *new_dog(char *name, float age, char *owner);
