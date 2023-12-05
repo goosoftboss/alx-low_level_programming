ad8bd32c706624c41775c68e8ec8e0c48994ccbe
@goosoftboss <36715091+goosoftboss@users.noreply.github.com>
2023-12-05 12:42:59 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,49 +1,48 @@
 #include "main.h"
 
 /**
- * print_times_table - print the N times table, starting with 0.
- * @n: time table size
- * Return: none.
+ * print_times_table - prints the n times table starting with 0 between 0-15
+ * @n: Integer
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 void print_times_table(int n)
 {
-	int i, a, j, rev, len, p = 0;
+	int i, j, m, d;
 
-	if (n >= 0 && n <= 15)
+	if ((n < 15) && (n >= 0))
 	{
-		for (i = 1; i <= n ; ++i)
+		for (i = 0; i <= n; i++)
 		{
-			_putchar ('0');
-			_putchar (',');
-			for (a = 1; a <= 3; ++a)
-				_putchar (' '); }
-		_putchar ('0');
-		for (i = 1; i <= n; ++i)
-		{
-			_putchar ('\n');
-			for (j = 0; j <= n; ++j)
+			for (j = 0; j <= n; j++)
 			{
-				if (j == 0)
-					_putchar ('0');
-				else
+				d = i * j;
+				if (j != 0)
 				{
-					p = i * j;
-					rev = len = 0;
-					while (p > 0)
+					_putchar(44);
+					_putchar(32);
+					if (d <= 9)
 					{
-					rev = (rev * 10) + (p % 10);
-					++len;
-					p = p / 10; }
-					_putchar (',');
-					_putchar (' ');
-					if (len < 3)
-					{
-					_putchar (' ');
-					if (len < 2)
-					_putchar (' '); }
-					while (len > 0)
-					{
-						_putchar ('0' + rev % 10);
-						rev = rev / 10;
-						--len; }}}}
-	_putchar ('\n'); }}
+						_putchar(32);
+						_putchar(32);
+					}
+				}
+
+				if (d > 99)
+				{
+					m = d / 10;
+					_putchar('0' + m / 10);
+					_putchar('0' + m % 10);
+				}
+				else if ((d >= 10) && (d <= 99))
+				{
+					_putchar(32);
+					_putchar('0' + d / 10);
+				}
+				_putchar('0' + d % 10);
+			}
+			_putchar('\n');
+		}
+	}
+}
